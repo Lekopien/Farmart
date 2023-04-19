5dee36948a1da5fa2d17c112832243e4a8a2a980
Robert <robethuku11@gmail.com>
2023-04-19 23:45:05 +0300

    Add the use effect hook to mornitor the input fields

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -4,6 +4,8 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const ADDRESS_REGEX = /^(?:\+254|254|0)((1|7)(?:(?:[0–9][0–9])|(?:[0–9][0–9][0–9]))[0–9]{6})$/;
 
 
 const SignUp = () => {
@@ -15,9 +17,17 @@ const SignUp = () => {
   const [validName, setValidName] = useState(false);
   const [userFocus, setUserFocus] = useState(false);
 
+  const [email, setEmail] = useState('');
+  const [validEmail, setValidEmail] = useState(false);
+  const [EmailFocus, setEmailFocus] = useState(false);
+
+  const [address, setUserAddress] = useState('');
+  const [validAddress, setValidAddress] = useState(false);
+  const [AddressFocus, setAddressFocus] = useState(false);
+
   const [password, setPassword] = useState('');
-  const [validPwd, setValidPwd] = useState(false);
-  const [passworddFocus, setPasswordFocus] = useState(false);
+  const [validPassword, setValidPassword] = useState(false);
+  const [passwordFocus, setPasswordFocus] = useState(false);
 
   const [confirmPassword, setConfirmPassword] = useState('');
   const [validMatch, setValidMatch] = useState(false);
@@ -26,12 +36,37 @@ const SignUp = () => {
   const [errMsg, setErrMsg] = useState('');
   const [success, setSuccess] = useState(false);
 
+  useEffect(() => {
+    userRef.current.focus();
+}, [])
+
+useEffect(() => {
+    setUser(USER_REGEX.test(user));
+}, [user])
+
+useEffect(() => {
+  setEmail(EMAIL_REGEX.test(address));
+}, [email])
+
+useEffect(() => {
+  setUserAddress(ADDRESS_REGEX.test(email));
+}, [email])
+
+useEffect(() => {
+    setPassword(PASSWORD_REGEX.test(password));
+    setConfirmPassword(password === confirmPassword);
+}, [password, confirmPassword])
+
+useEffect(() => {
+    setErrMsg('');
+}, [user, password, confirmPassword])
+
 
 
   return (
-    <div>
+    <section>
 
-    </div>
+    </section>
   )
 }
 
