5c4139927aa603cd122aa9511c5d3800629273da
Lewis1Ndungu <lewis.ndungu@student.moringaschool.com>
2023-04-19 13:27:34 +0300

    Generated cart resource

---
+++ db/migrate/20230419101054_create_carts.rb
@@ -0,0 +1,13 @@
+class CreateCarts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :carts do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :animal, null: false, foreign_key: true
+      t.references :land, null: false, foreign_key: true
+      t.integer :quantity
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
