66a11aaac8ad05df7240d49260bf84f4f31befea
Robert <robethuku11@gmail.com>
2023-04-19 23:11:57 +0300

    Create the signup component and export it

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -3,6 +3,18 @@ import { faCheck,faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+
+const SignUp = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+export default SignUp;
 
 
