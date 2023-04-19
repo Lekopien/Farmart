3eab53899a875db07fc265888d12013f8096a27f
Robert <robethuku11@gmail.com>
2023-04-19 23:08:47 +0300

    Create the user and password regex

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,1 +1,8 @@
 import{useRef,useState,useEffect } from "react"
+import { faCheck,faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+
