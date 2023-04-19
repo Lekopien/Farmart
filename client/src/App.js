81cc8b748ef9282c56cf4edf4659b89ee18ecdb6
karemu1842call <victorkinoti03@gmail.com>
2023-04-20 02:05:02 +0300

    animals and land form

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Products from "./components/Products";
 import Lease from "./components/Lease";
+import AnimalsForm from "./utilis/AnimalsForm"
+import LandForm from "./utilis/LandForm";
+import FarmerProd from "./components/FarmerProd";
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/about" element={<About />} />
         <Route path="/lease" element={<Lease />} />
+        <Route path="/animalsform" element={<AnimalsForm />} />
+        <Route path="/landform" element={<LandForm />} />
+        <Route path="/farmerprod" element={<FarmerProd />} />
       </Routes>
       <Footer />
     </>
