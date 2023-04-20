9cbf467c24f92f115f6ab6169305c09bfe788f30
CALVINCE <calvince.ogwel@gmail.com>
2023-04-20 15:28:27 +0300

    changed login redirect path

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -11,7 +11,7 @@ const Login = ()  => {
   const navigate = useNavigate();
   const location = useLocation()
 
-  const from = location.state?.from?.pathname || "/"
+  const from = location.state?.from?.pathname || "/products"
 
   const userRef = useRef();
   const errRef  = useRef();
